ab8e3f7eda117dab96e47c288b4c211dc661ef54
Saikat Taleb <saikamda@fit.cvut.cz>
2023-06-17 23:56:27 +0200

    migrated to next js 13

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,18 @@
-import Footer from '../components/footer'
-import 'bootstrap/dist/css/bootstrap.css'
-import Header from '../components/header'
-import { CartProvider } from '../components/cart/cartcontext'
-import { SessionProvider } from "next-auth/react"
-import Body from '../components/body'
-
-
+import Footer from "../components/footer";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "../components/header";
+import { CartProvider } from "../components/cart/cartcontext";
+import { SessionProvider } from "next-auth/react";
+import Body from "../components/body";
 
 export default function App({ session, Component, pageProps }) {
-
   return (
     <SessionProvider session={session}>
       <CartProvider>
-        <Header/>
-         <Body Component={Component} pageProps={pageProps} />
-        <Footer/>
+        <Header />
+        <Body Component={Component} pageProps={pageProps} />
+        <Footer />
       </CartProvider>
     </SessionProvider>
-  )
+  );
 }
